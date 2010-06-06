56f25c9abe901e68427b839b4017628e833bbd7d
gordo <gordo@yugo.at>
2010-06-06 23:06:09 +0200

    changed directory structure of www

--- www/number.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('settings.php');
-mysql_connect($host,$user,$password) or die(mysql_error());
-mysql_select_db($db) or die(mysql_error());
-
-echo $NumberOfResults = mysql_num_rows(mysql_query("SELECT * FROM users"));
-?>
--- www/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "localhost";
-$user = "gordo";
-$password = "mockshow";
-$db = "web20suicide";
-?>
