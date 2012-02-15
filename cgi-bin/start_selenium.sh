783926a68bdb1699e30a3a29cf03d3a4e60b313b
Fresco Gamba <gordo@wormweb.nl>
2012-02-15 16:53:21 +0000

    removing symlink cgi-bin

--- cgi-bin/start_selenium.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# read the current id 
-JOBID=$1
-
-java -jar /usr/lib/cgi-bin/selenium-server1_0_3.jar -port ${JOBID} -interactive -forcedBrowserMode "*firefox /usr/lib/firefox-3.6.3/firefox" -log "/tmp/selenium${JOBID}.log"
--- cgi-bin/wall.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "yo9oo";
-?>
